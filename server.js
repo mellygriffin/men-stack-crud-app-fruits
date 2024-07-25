efd7725508bed969fe70d1e83239c15c89fbd352
MellGriffin <missmelissagriffin@gmail.com>
2024-07-25 13:26:29 -0500

    Add route for the root route and test

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-// Starter Code //
+//------------- Starter Code -----------------//
+
+//Here's where we import modules - Express
 const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(3000, () => { //listens for anytype of requests
     console.log("Listening on Port 3000")
 })
+
+//GET '/' to the root route - AKA landing page
+app.get('/', async (req, res) => {
+    res.send("Hello, friend!");
+});
+
+//------------- Blah Blah Blah -----------------//
