f913750f22df4b4f3c991ba7c26bff76bc95ec43
MellGriffin <missmelissagriffin@gmail.com>
2024-07-25 13:10:18 -0500

    Created server file and README

---
+++ README.md
@@ -0,0 +1,1 @@
+Creating a fruit app!
---
+++ server.js
@@ -0,0 +1,8 @@
+// Starter Code //
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000")
+})
